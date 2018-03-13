f8af99e732105f1e9f4a6168a602815939b6ac22
jzilcov@gmail.com <adjoq401293uriawdf>
2018-03-13 16:54:57 +0000

    First Commit

---
+++ ClearBank.DeveloperTest.Tests/Tests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClearBank.DeveloperTest.Types;
+using ClearBank.DeveloperTest.Services;
+using NUnit.Framework;
+using Moq;
+using ClearBank.DeveloperTest.Data;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PaymentService_MakeChapsPayment_Success()
+        {
+            // Arrange
+            var mockDataStore = new Mock<IDataStore>(MockBehavior.Strict);
+            mockDataStore.Setup(p => p.GetAccount("222")).Returns(new Account
+            {
+                AccountNumber = "222",
+                Balance = 300m,
+                Status = AccountStatus.Live,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps
+            });
+            mockDataStore.Setup(p => p.GetAccount("111")).Returns(new Account
+            {
+                AccountNumber = "111",
+                Balance = 200m,
+                Status = AccountStatus.Live,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps
+            });
+
+
+            var ps = new PaymentService(mockDataStore.Object);
+
+            var mpr = new MakePaymentRequest() {
+                CreditorAccountNumber = "111",
+                DebitorAccountNumber = "222",
+                Amount = 100m,
+                PaymentDate = DateTime.Now,
+                PaymentScheme = PaymentScheme.Chaps
+            };
+
+
+            // Act
+            var result = ps.MakePayment(mpr);
+
+            // Assert
+            Assert.AreEqual(true, result.Success);
+        }
+
+        [Test]
+        public void PaymentService_MakeFasterPaymentWithInsufficientFunds_Fail()
+        {
+            // Arrange
+            var mockDataStore = new Mock<IDataStore>(MockBehavior.Strict);
+            mockDataStore.Setup(p => p.GetAccount("222")).Returns(new Account
+            {
+                AccountNumber = "222",
+                Balance = 100m,
+                Status = AccountStatus.Live,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments
+            });
+            mockDataStore.Setup(p => p.GetAccount("111")).Returns(new Account
+            {
+                AccountNumber = "111",
+                Balance = 200m,
+                Status = AccountStatus.Live,
+                AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments
+            });
+
+
+            var ps = new PaymentService(mockDataStore.Object);
+
+            var mpr = new MakePaymentRequest()
+            {
+                CreditorAccountNumber = "111",
+                DebitorAccountNumber = "222",
+                Amount = 300m,
+                PaymentDate = DateTime.Now,
+                PaymentScheme = PaymentScheme.Chaps
+            };
+
+
+            // Act
+            var result = ps.MakePayment(mpr);
+
+            // Assert
+            Assert.AreEqual(false, result.Success);
+        }
+    }
+}
---
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -0,0 +1,109 @@
+﻿using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Types;
+using System;
+using System.Configuration;
+
+namespace ClearBank.DeveloperTest.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private IDataStore _datastore;
+
+        public PaymentService(IDataStore datastore)
+        {
+            _datastore = datastore;
+        }
+
+        public MakePaymentResult MakePayment(MakePaymentRequest request)
+        {
+            Account account = GetAccount(request.DebitorAccountNumber) ?? null;
+
+            var result = new MakePaymentResult();
+
+            if (IsPaymentAllowed(request, account))
+            {
+                try
+                {
+                    account.Balance -= request.Amount;
+                    // A call to do the actual transfer should be here
+                    result.Success = true;
+                    _datastore.UpdateAccount(account);                    
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("An error occurred while making the payment: '{0}'", e);
+                }                
+            }
+            else
+            {
+                result.Success = false;
+            }
+
+            return result;
+        }
+        
+        private bool IsPaymentAllowed(MakePaymentRequest request, Account account)
+        {            
+            
+            switch (request.PaymentScheme)
+            {
+                case PaymentScheme.Bacs:
+                    if (account == null)
+                    {
+                        return false;
+                    }
+                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
+                    {
+                        return false;
+                    }
+                    else if (account.Status == AccountStatus.Live)
+                    {
+                        return true;
+                    }
+                    break;
+
+                case PaymentScheme.FasterPayments:
+                    if (account == null)
+                    {
+                        return false;
+                    }
+                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
+                    {
+                        return false;
+                    }
+                    else if (account.Balance < request.Amount  && account.Status == AccountStatus.Live)
+                    {
+                        return true;
+                    }
+                    break;
+
+                case PaymentScheme.Chaps:
+                    if (account == null)
+                    {
+                        return false;
+                    }
+                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
+                    {
+                        return false;
+                    }
+                    else if (account.Status == AccountStatus.Live)
+                    {
+                        return true;
+                    }
+                    break;
+
+                default:
+                    return false;                    
+            }
+            
+            return false;
+        }
+
+        private Account GetAccount(string DebitorAccountNumber)
+        {            
+            Account account = _datastore.GetAccount(DebitorAccountNumber);
+
+            return account;
+        }
+    }
+}
